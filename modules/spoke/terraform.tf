b19ccf51d599409bb85d07ac79ef6ce6a3b9bce0
Sam Cogan <mail@samcogan.com>
2026-01-26 15:11:25 +0000

    chore: remove unused azurerm provider from spoke module

--- modules/spoke/terraform.tf
+++ modules/spoke/terraform.tf
@@ -6,10 +6,5 @@ terraform {
       source  = "Azure/azapi"
       version = ">= 2.0.0, < 3.0.0"
     }
-    # tflint-ignore: terraform_unused_required_providers - Required for provider inheritance by AVM submodules
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 5.0.0"
-    }
   }
 }
